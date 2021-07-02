f70cf1ee8b6da9c3e9dd6daa3d43a9b743146f86
危唯 <526710585@qq.com>
2021-07-02 19:21:28 +0800

    1.存一下

--- build/methods/getconf.js
+++ build/methods/getconf.js
@@ -3,8 +3,12 @@ const config = require('../config');
 const getFilesName = require('./getFilesName');
 
 function getconf(){
+  // 根据目录
   const fileArr = getFilesName(path.resolve(__dirname, '../../src/game/'));
+  console.log(fileArr);
+  // 分开环境版本 + 添加版本名称
 
+  //输出配置
 }
 
 getconf()
